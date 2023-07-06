9fc0170610e311e816fa1533933a16fdf0e9e142
Vishal Jadav <124347933+VishalJadav23@users.noreply.github.com>
2023-07-06 22:19:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const Connect = require("./MongoConect")
+const UserRouter = require("./User/UserRouter")
+Connect()
+
+const App = express()
+
+App.use(express.json())
+
+
+App.get("/", (req, res) => {
+    return res.status(200).send({ message: "Success" })
+})
+
+App.use("/user", UserRouter)
+
+App.use("/view", express.static('views'))
+
+App.listen(5000, () => {
+    console.log("server started");
+})
+
+
